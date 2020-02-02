a4865bb893fe26c8b838d5d8efb0a0c92597f4d7
EC2 Default User <ec2-user@ip-172-31-23-162.us-east-2.compute.internal>
2020-02-02 21:22:41 +0000

    signup user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
   end
   
   def create 
-    @user = User .new(params[:user])
+    @user = User .new(user_params)
     
     if @user .save
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to @user
     else
       render "new"
     end 
@@ -19,7 +21,7 @@ class UsersController < ApplicationController
     
     
     def user_params
-      params .require(:user) .permit(:name, :email, :passwprd, 
+      params .require(:user) .permit(:name, :email, :password, 
                                      :password_confirmation)
     end
 end
