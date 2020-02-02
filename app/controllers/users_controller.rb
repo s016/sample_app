5c92d63b22f0ada9e7d3a78c00ce3d71d39c59d2
EC2 Default User <ec2-user@ip-172-31-23-162.us-east-2.compute.internal>
2020-02-02 13:08:46 +0000

    set errors_messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
   def new
     @user = User .new
   end
+  
+  def create 
+    @user = User .new(params[:user])
+    
+    if @user .save
+    else
+      render "new"
+    end 
+  end 
+  private
+    
+    
+    def user_params
+      params .require(:user) .permit(:name, :email, :passwprd, 
+                                     :password_confirmation)
+    end
 end
