cd63e167f82a1a61ef41e26cb089539d3683535e
EC2 Default User <ec2-user@ip-172-31-23-162.us-east-2.compute.internal>
2020-02-04 16:37:55 +0000

    set login url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   
   def create 
     user = User .find_by(email: params[:session][:email] .downcase)
-    if user && user .authenticate(params[:session][password])
+    if user && user .authenticate(params[:session][:password])
       log_in user
-      redeirect_to user
+      redirect_to user
       
     else
         flash .now[:danger] = 'Invalid email/password combination' 
